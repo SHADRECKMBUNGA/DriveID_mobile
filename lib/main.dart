d4f15aea224b5b0d11489d21760674ba8c6d73c9
Victor-Magreta <bed-com-41-22@unima.ac.mw>
2026-04-19 11:45:54 +0200

    Update main.dart
    changed home page from traffic dashboard to driver dashboard

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:driveid_app/features/driver/driver_dashboard.dart';
 import 'package:flutter/material.dart';
 import 'package:app_links/app_links.dart';
 import 'core/config/supabase_config.dart';
@@ -90,7 +91,7 @@ class _MyAppState extends State<MyApp> {
       initialRoute: '/',
       routes: {
         '/': (context) => LoginScreen(),
-        '/login': (context) => DashboardScreen(),
+        '/login': (context) => DriverDashboard(),
       },
     );
   }
